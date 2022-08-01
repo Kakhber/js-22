b3e23956edca1451babdb77ccf29d07311c5d30c
kakhber <beridzekakha@gmail.com>
2022-08-02 00:10:19 +0400

    add js

--- app.js
+++ app.js
@@ -5,3 +5,29 @@ const board = [
     [ 0, 0, 0, 0, 0 ],
     [ 0, 0, 0, 0, 0 ]
 ];
+for(let i = 0; i < board.length; i++){
+    for(let j = 0; j < board[i].length; j++){
+        //console.log(board[i][j])
+    }
+}
+
+let timer = document.getElementById('timer');
+let realTimer = 15;
+let IntervalCounter = 600;
+
+let interval = setInterval(startTimer, IntervalCounter);
+
+function startTimer() {
+    realTimer--;
+    timer.textContent = realTimer;
+    if (realTimer < 1) {
+        endGame();
+    }
+}
+function stopTimer(){
+
+}
+function endGame(){
+    alert('Game Over');
+    clearInterval(interval)
+}
